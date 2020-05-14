85a0857a85f2f4829d4ec115fd0ac26cd7d3114b
Nannavedel <nanna.vedel@gmail.com>
2020-05-14 14:36:10 +0200

    Datepicker: Tilføjet lidt JS
    Js kode: første del = "active" i js (under "functions") samt i css.
    
    Når der klikkes på datoen skulle en blok dukke op nedenunder.
    
    --> Det virker dog ikke helt endnu.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const date_picker_element = dokument.querySelector('.date-picker');
+const selected_date_element = document.querySelector('.date-picker .selected-date');
+const dates_element = document.querySelector('date-picker .dates');
 
+// EVENT LISTENERS
+date_picker_element.addEventListener('click', toggleDatePicker);
+
+//FUNCTIONS
+function toggleDatePicker (e) {
+  date_element.classList.toggle('active');
+}
