45e43392d20b4735a94db4c3aa00ac907897208a
Nannavedel <nanna.vedel@gmail.com>
2020-05-14 14:44:22 +0200

    Datepicker: Små ændringer/noter
    Små ændringer i JS samt noter i html.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const date_picker_element = dokument.querySelector('.date-picker');
+const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('date-picker .dates');
 
@@ -7,5 +7,5 @@ date_picker_element.addEventListener('click', toggleDatePicker);
 
 //FUNCTIONS
 function toggleDatePicker (e) {
-  date_element.classList.toggle('active');
+  dates_element.classList.toggle('active');
 }
