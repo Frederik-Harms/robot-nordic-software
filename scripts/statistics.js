2ccedf4e9dfd1eb520aa0d0a1ff4f3c487cbbd0d
idabille <60378712+idabille@users.noreply.github.com>
2020-05-20 17:10:18 +0200

    størrelse og margin rettet på graf

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -6,7 +6,7 @@
     Chart.defaults.global.defaultFontColor = '#000000';
 
     let massPopChart = new Chart(myChart, {
-      type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      type:'bar', // fungere også som: bar, horizontalBar, pie, line, doughnut, radar, polarArea
       data:{
         labels:['Folie', 'Pap', 'Plastik', 'Papir', 'Klistermærker', 'Metal'],
         datasets:[{
@@ -19,7 +19,7 @@
             35,
             75
           ],
-          //backgroundColor:'green',
+          //baggrunden på søjlerne kan også hedde fx backgroundColor:'green',hvis der ønskes én enkelt farve
           backgroundColor:[
             '#7F8282',
             '#C3C7C8',
@@ -32,20 +32,21 @@
           borderWidth:1,
          
           hoverBorderWidth:3,
-          hoverBorderColor:'#F15C22'
+          hoverBorderColor:'#F15C22' //styling af søjlerne/dataen
         }]
       },
       options:{
         title:{
-          display:true,
-          text:'STATISTIKKER',
-          fontSize:25
+          display:true, //sættes til true, så den ses konstant
+          text:'EMBALLAGE FORBRUG OVERSIGT',
+          fontSize:10
         },
         legend:{
-          display:true,
-          position:'right',
+          display:false, //sættes til false, da den ikke skal ses konstant, kun når musen kommer over
+          position:'right bottom',
           labels:{
-            fontColor:'#000'
+            fontColor:'#F15C22'
+			  
           }
         },
         layout:{
