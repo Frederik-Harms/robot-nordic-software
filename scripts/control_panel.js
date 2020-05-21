2e163a078d571d021f45585d0744d33d56ae931a
idabille <60378712+idabille@users.noreply.github.com>
2020-05-21 18:18:48 +0200

    Nyt slideshow virker ikke, selvom det får nyt navn

--- scripts/control_panel.js
+++ scripts/control_panel.js
@@ -28,6 +28,35 @@ alert();
 
 	window.onload = changeImg;
 
+var i = 0; // Start point
+	var images2 = [];
+	var time = 3000;
+alert();
+	// Image List
+	images2[0] = '../assets/img/statusline_show/status7.png';
+	images2[1] = '../assets/img/statusline_show/status6.png';
+	images2[2] = '../assets/img/statusline_show/status5.png';
+	images2[3] = '../assets/img/statusline_show/status4.png';
+    images2[4] = '../assets/img/statusline_show/status3.png';
+    images2[5] = '../assets/img/statusline_show/status2.png';
+    images2[6] = '../assets/img/statusline_show/status1.png';
+    images2[7] = '../assets/img/statusline_show/status0.png';
+
+	// Change Image
+	function changeImg(){
+		document.slide.src = images[i];
+
+		if(i < images.length - 1){
+			i++;
+		} else {
+			i = 0;
+		}
+
+		setTimeout("changeImg()", time);
+	}
+
+	window.onload = changeImg;
+
 //Status over pakkede paller, vises i en alert 
 function CustomAlert(){
     this.render = function(dialog){
@@ -44,7 +73,7 @@ function CustomAlert(){
         document.getElementById('dialogboxbody').innerHTML = dialog;
         document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
     }
-	/**lukker boxen igen**/
+	/**lukker boxen igen med ok knappen**/
 	this.ok = function(){
 		document.getElementById('dialogbox').style.display = "none"; 
 		document.getElementById('dialogoverlay').style.display = "none"; 
