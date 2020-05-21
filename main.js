4d0e68222e0c18308d483cc840f88409ec1ad2f7
Nannavedel <nanna.vedel@gmail.com>
2020-05-21 09:38:49 +0200

    Datepicker: Boks virker
    Havde glemt et enkel punktum i:
    const dates_element = document.querySelector('.date-picker .dates');
    
    Problem løst.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element = document.querySelector('date-picker .dates');
+const dates_element = document.querySelector('.date-picker .dates');
 
 // EVENT LISTENERS
 date_picker_element.addEventListener('click', toggleDatePicker);
@@ -16,7 +16,7 @@ function toggleDatePicker (e) {
 
 //HJÆLPE FUNKTIONER
 function checkEventPathForClass (path, selector) { // vi tjekker "event path", så denne kode registrer hvor vi klikker, og udfører et "event" udfra vores klik
-  for (let i = 0; i < path.lenght; i++) {
+  for (let i = 0; i < path.lenght; i++) { // path.lenght - betyder at når man klikker på pilene i boksen, forsvinder kassen ikke hver eneste gang 
     if (path[i].classList && path[i].classList.contains(selector)) {
       return true;
     }
