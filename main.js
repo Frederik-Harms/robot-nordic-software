cf3113b6236399eaf0c2037a6b620eb684231bb2
Nannavedel <nanna.vedel@gmail.com>
2020-05-21 11:47:33 +0200

    Datepicker: js psth.length
    Path.length, skulle gør at boksen IKKE forsvinder når man klikker på next-pilen... men det gør den.. ved ikke hvad problemet kan være...

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ date_picker_element.addEventListener('click', toggleDatePicker);
 
 //FUNKTIONER
 function toggleDatePicker (e) {
+  console.log(e.path); //bruges for at inspicere i js koden i browser
+  
   if (!checkEventPathForClass(e.path, 'dates')) {
-      dates_element.classList.toggle('active'); //ved klik på dato, skal en blok komme frem - det gør den ikke??
+      dates_element.classList.toggle('active'); //ved klik på dato, kommer dropdown-blok frem
   }
 }
 
 //HJÆLPE FUNKTIONER
 function checkEventPathForClass (path, selector) { // vi tjekker "event path", så denne kode registrer hvor vi klikker, og udfører et "event" udfra vores klik
-  for (let i = 0; i < path.lenght; i++) { // path.lenght - betyder at når man klikker på pilene i boksen, forsvinder kassen ikke hver eneste gang
+  for (let i = 0; i < path.lenght; i++) { // path.lenght - betyder at når man klikker på pilene i boksen, forsvinder kassen ikke hver eneste gang --> virker ikke pt??
     if (path[i].classList && path[i].classList.contains(selector)) {
       return true;
     }
