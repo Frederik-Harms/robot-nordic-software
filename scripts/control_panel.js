1e0a0514cb6e4df332d5589a5c7fa9bfdf8bc620
idabille <60378712+idabille@users.noreply.github.com>
2020-05-24 22:26:00 +0200

    VAR lavet om til LET

--- scripts/control_panel.js
+++ scripts/control_panel.js
@@ -1,7 +1,7 @@
 // JavaScript Document
-var i = 0;   // Dette er slideshowet start punkt, altså den starter fra 0
-	var images = [];
-	var time = 3000; //dette er tiden mellem billedernes skift, sat i millisekunder (dette er 3 sek)
+let i = 0;   // Dette er slideshowet start punkt, altså den starter fra 0
+	let images = [];
+	let time = 3000; //dette er tiden mellem billedernes skift, sat i millisekunder (dette er 3 sek)
 
 	// Image List sat op i array
 	images[0] = '../assets/img/palle_show/palle7.jpg';
