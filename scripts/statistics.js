ab05b9fb4da8205f622be961d304c9906e41d9b4
MarkReberholt <60346830+MarkReberholt@users.noreply.github.com>
2020-05-25 02:12:05 +0200

    clean-up i mappen og små justeringer på statistics.htmll

--- scripts/statistics.js
+++ scripts/statistics.js
@@ -1,11 +1,11 @@
-// JavaScript Document		  
+// JavaScript Document
   let myChart = document.getElementById('myChart').getContext('2d'); //Når man laver en canvas som i dette tilfælde, vil man have en context, her sættes den som 2d
 
     // Global Options
     Chart.defaults.global.defaultFontFamily = 'Titillium Web';
     Chart.defaults.global.defaultFontSize = 15;
     Chart.defaults.global.defaultFontColor = '#000000';
-   
+
 //Her laves en ny variable, med mychart etableret i. Den kaldes emballagechart da den indeholder data over emballage
     let emballageChart = new Chart(myChart, {
       type:'bar', // fungere også som: bar, horizontalBar, pie, line, doughnut, radar, polarArea
@@ -43,11 +43,11 @@
           fontSize:10
         },
         legend:{
-          display:false, //Dette er en lille firkant derfor tæller hvad der er på grafen. Den sættes til false. Den ses ikke konstant, da det ikke passer godt til formen
+          display:true, //Dette er en lille firkant derfor tæller hvad der er på grafen. Den sættes til false. Den ses ikke konstant, da det ikke passer godt til formen
           position:'right bottom',
           labels:{
             fontColor:'#F15C22'
-			  
+
           }
         },
         layout:{ //Udseeende på selve grafen
@@ -56,11 +56,11 @@
             right:0,
             bottom:0,
             top:50
-			
+
           }
         },
         tooltips:{
-          enabled:false
+          enabled:true
         }
       }
     });
