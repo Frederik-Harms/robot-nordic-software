ea5f81a02da00bc5386f2053198f05e2f1c75fdc
MarkReberholt <60346830+MarkReberholt@users.noreply.github.com>
2020-05-25 12:59:03 +0200

    kommentarer

--- scripts/login.js
+++ scripts/login.js
@@ -3,12 +3,12 @@
 UserID and Password*/
 function pasuser(form) {
 
-  //her er der brugt if else-loops, hvor brugernavnet og adgangskoden tjekkes om det er true. her er både adgangskode og brugernavn angivet til, at være 'test'. Hvis det er true, og man har indtastet de korrekte oplysninger, er der sat en location på llogin-knappen, som fører brugeren til forsiden. 
+  //her er der brugt if else-statements, hvor brugernavnet og adgangskoden tjekkes om det er true. her er både adgangskode og brugernavn angivet til, at være 'test'. Hvis det er true, og man har indtastet de korrekte oplysninger, er der sat en location på login-knappen, som fører brugeren til forsiden.
 if (form.id.value=="test") {
 if (form.pass.value=="test") {
 location="../forside.html"
 
-//hvis adgangskode og eller brugernavn er forkert, kommer der en alert om, at enten brugernavn eller adgangskode er forkert, også får man ikke lov til, at kommer ind på location.
+//hvis adgangskode og eller brugernavn er falsk, kommer der en alert om, at enten brugernavn eller adgangskode er forkert, og så får man ikke lov til, at kommer ind på location.
 } else {
 alert("Forkert Adgangskode")
 }
--- scripts/skabeloner.js
+++ scripts/skabeloner.js
@@ -1,19 +1,22 @@
-// dette er containeren som definerer hvilket step man er på, samt hvilket step som er default-steppet, altså start-steppet. Den definerer samtidig hvor langt man er i processen ved at vise de enkelt steps. der er benyttet let i stedet for var, da det er mere struktureret.
+// dette er containeren som definerer hvilket step man er på, samt hvilket step som er default-steppet, altså start-steppet. Den definerer samtidig hvor langt man er i processen ved at vise de enkelt steps. der er benyttet let i stedet for var, da det er mere struktureret, da funktionen ikkemkører, før den skal bruges, da var bliver kørt hele tiden
+
 let currentTab = 0; // Den tab (side) som man starter på,m er defineret til, at være den første tab, altså currentTab = 0
 
 showTab(currentTab); // Denne viser hvilket tab man er på, altså den nuværende tab.
 
 
-//denne funktion bestemmer, hvad der skall vises på de forskellige tabs
+//denne funktion bestemmer, hvad der skal vises på de forskellige tabs
 function showTab(n) {
 
 
-// Denne let er et array, (document.getElementsByClassName), som bestemmer hvad der skal vises, ved at gå hele html'en igennem, for at finde den rigtige class eller id. På den måde samles der en liste, over de udvalgte dele, som skal udføres i den givne funktion, altså de forskellige steps.
+// Denne let er et array, (document.getElementsByClassName), som bestemmer hvad der skal vises, ved at gå DOM'en igennem, for at finde den rigtige class eller id. På den måde samles der en liste, over de udvalgte dele, som skal udføres i den givne funktion, altså de forskellige steps.
+
+//Samtidig bestemmes der, at på n, altså nuværende tab, skal display:block, resten blir display:none.
   let x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
 
 
-//der bliver her brugt if-else loops til at validere, om man har skrevet noget i input felterne, for hvis man ikke har,. kan man ikke gå videre. Det sker ved, at den ændrer i tab'en til henholdsvis -display: none- hvis det ikk er true, eller -display: inline block- hvis det er true, og så går man videre
+//der bliver her brugt if-else loops til at bestemme, hvilken knap som skal vises på tab'en. Her skal er der bestemt, at hvis man er på den første tab, skal tilbage-knappen ikke vises, men hvis man ikke er, så skal den vises Det sker ved, at den ændrer i tab'en til henholdsvis -display: none- hvis det ikk er true, eller -display: inline - hvis det er true, og så går man videre
 
 
 //BOOLEANS DATA  SES F.EKS HER I IF STATEMENTET, HVOR DEN VENDER TILBAGE MED VALUE TRUE ELLER FALSE.
@@ -23,18 +26,18 @@ function showTab(n) {
     document.getElementById("prevBtnskabelon").style.display = "inline";
   }
 
-  //Her gællder det samme, som i ovenstående eksempel, her er der bare defineret en længde på form-wizarden, så den ved, at når man er kommet to steps igennem, så viser den knappen "start" i stedet for ""videre".
+  //Her gælder det samme, som i ovenstående eksempel, her er der bare defineret en længde på form-wizarden, så den ved, at når man er kommet to steps igennem, så viser den knappen "start" i stedet for ""videre".
   if (n == (x.length - 2)) {
     document.getElementById("nextBtnskabelon").innerHTML = "START";
   } else {
     document.getElementById("nextBtnskabelon").innerHTML = "VIDERE";
   }
 
-//denne viser det korrekte step og tab man er på
+//denne viser det korrekte step  man er på i forhold til sin tab
   fixStepIndicator(n)
 }
 
-//nedenfor, er der brugt function by declarations. altså hvilken kode som skal vises inden for de forskelllige curly-brackets
+//nedenfor, er der brugt function by declarations. altså hvilken kode som skal vises inden for de forskellige curly-brackets
 function nextPrev(n) {
 
   // her bliver tab'en tildelt X-declaration via et array. "document.getElementsByClassName".
@@ -50,15 +53,15 @@ function nextPrev(n) {
   currentTab = currentTab + n;
 
 
-//hvis(if) du er nået til den sidste tab, så køre den, den sidste kode
+//hvis(if) du er nået til den sidste tab, så køre den, den sidste kode, og viser den sidste tab med de korrekte knapper
   if (currentTab >= x.length) {
 
-// return false gør, at man ikke kommer videre til næste tab, men derimod den side man har angivet i "action" i regForm'en.  i dette tilfælde kontrolpanelet, så der lægges bare en sti hen till kontrolpanel-html'en.
+// return false gør, at man ikke kommer videre til næste tab, men derimod den side man har angivet i "action" i regForm'en ved submit-knappen. I dette tilfælde kontrolpanelet, så der lægges bare en sti hen til kontrolpanel-html'en.
     document.getElementById("regFormskabelon").submit();
     return false;
   }
 
-//hvis if-statementet ikke er true, går den derimod videre til nsæte tab.
+//hvis if-statementet ikke er true, går den derimod videre til næste tab.
   showTab(currentTab);
 }
 
@@ -70,8 +73,8 @@ function validateForm() {
   x = document.getElementsByClassName("tab");
   y = x[currentTab].getElementsByTagName("input");
 
-  // DETTE ER ET FOR LOOP, HVOR "I" BLIVER TILDELT 0. HVIS "I" ER MINDRE END CURRENT TAB LENGTH (ALTSÅ INGENTING), SÅ SKAL DEN FORTAGE SIG:
-  for (i = 0; i < y.length; i++) {
+    // DETTE ER ET FOR LOOP, HVOR "I" BLIVER TILDELT 0. HVIS "I" ER MINDRE END CURRENT TAB LENGTH (ALTSÅ INGENTING), SÅ SKAL DEN FORTAGE SIG:
+    for (i = 0; i < y.length; i++) {
 
  // AF DETTE IF STATEMENT, HVOR SAMMENLIGNINGS OPRETOREN BESKRIVER AT "CURRENT TAB" ER DET SAMMEN SOM INGENTING (EMPTY).
     if (y[i].value == "") {
